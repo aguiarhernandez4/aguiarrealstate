91d23eea0887b69abf75c2ddfd484e5c548dcd43
Hernandez <aguiarhernandez4@gmail.com>
2025-08-05 16:58:10 -0300

    Atualização 05/08/2025 16:58

--- src/main/java/com/aguiarrealstate/first_spring_app/controller/CasasController.java
+++ src/main/java/com/aguiarrealstate/first_spring_app/controller/CasasController.java
@@ -9,5 +9,30 @@ public class CasasController {
     public String lista() {
         return "casas";
     }
+    
+    public class casa {
+        String endereco;
+        int preco;
+        String bairro;
+        String cidade;
+        String estado;
+        String cep;
+        String quartos;
+        String banheiros;
+        String garagem;
+        String metragem;
+        String descricao;
 
+        public void conferir_dados() {
+            if (endereco.length() == 0 || preco <= 200 || bairro.isEmpty() == true || cidade.isEmpty() == true || estado.isEmpty() == true || cep.isEmpty() == true || quartos.isEmpty() == true || banheiros.isEmpty() == true || garagem.isEmpty() == true || metragem.isEmpty() == true || descricao.isEmpty() == true) {
+                System.out.println("Dados inválidos, por favor verifique os campos.");
+
+                } 
+                else {
+                System.out.println("Dados válidos, casa cadastrada com sucesso!");
+
+                }
+        }
+        
+    }
 }
